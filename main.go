fcc62f785308ea3041a14a7b6e1f2aa1b3cdbe8d
Nathan Cordeiro <nathancribeiro13@hotmail.com>
2024-11-02 22:46:52 -0300

    commit :)

--- main.go
+++ main.go
@@ -28,39 +28,39 @@ func main() {
 		case 2:
 			readLog()
 		case 0:
-			fmt.Println("Saindo do programa")
+			fmt.Println("Leaving program")
 			os.Exit(0)
 		default:
-			fmt.Println("Não conheço este comando")
+			fmt.Println("Don't know this command")
 			os.Exit(-1)
 		}
 	}
 }
 
 func showIntroduction() {
-	name := "Nathan"
-	version := "23.2"
-	fmt.Println("Olá,", name)
-	fmt.Println("A versão desse programa é a", version)
+	name := "User"
+	version := "1.0"
+	fmt.Println("Hello,", name)
+	fmt.Println("The program version is", version)
 }
 
 func showMenu() {
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
+	fmt.Println("1- Start monitoring")
+	fmt.Println("2- View logs")
+	fmt.Println("0- Exit program")
 }
 
 func readCommand() int {
 	var readedCommand int
 	fmt.Scan(&readedCommand)
-	fmt.Println("O comando escolhido foi", readedCommand)
+	fmt.Println("The command chosen was", readedCommand)
 	fmt.Println("")
 
 	return readedCommand
 }
 
 func startMonitoring() {
-	fmt.Println("Monitorando...")
+	fmt.Println("Monitoring...")
 	fmt.Println("")
 
 	// websites := []string{"https://httpbin.org/status/404", "https://httpbin.org/status/200", "https://www.google.com.br"}
@@ -69,7 +69,7 @@ func startMonitoring() {
 
 	for i := 0; i < monitoring; i++ {
 		for i, website := range websites {
-			fmt.Println("Testando o site", i, ":", website)
+			fmt.Println("Testing the website", i, ":", website)
 			testWebsite(website)
 		}
 		time.Sleep(delay * time.Second)
@@ -83,14 +83,14 @@ func testWebsite(website string) {
 	resp, err := http.Get(website)
 
 	if err != nil {
-		fmt.Println("Ocorreu um erro:", err)
+		fmt.Println("An error occurred:", err)
 	}
 
 	if resp.StatusCode == 200 {
-		fmt.Println("Resposta: ", website, "foi carregado com sucesso!")
+		fmt.Println("Response: ", website, "was loaded successfully!")
 		registerLog(website, true)
 	} else {
-		fmt.Println("Resposta:", website, "está com problemas:", resp.StatusCode)
+		fmt.Println("Response:", website, "is in trouble:", resp.StatusCode)
 		registerLog(website, false)
 	}
 }
@@ -101,7 +101,7 @@ func readArchiveWebsites() []string {
 	archive, err := os.Open("websites.txt")
 
 	if err != nil {
-		fmt.Println("Ocorreu um erro:", err)
+		fmt.Println("An error occurred:", err)
 	}
 
 	reader := bufio.NewReader(archive)
@@ -126,10 +126,10 @@ func registerLog(website string, status bool) {
 	archive, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
-		fmt.Println("Ocorreu um erro:", err)
+		fmt.Println("An error occurred:", err)
 	}
 
-	archive.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + website + " | online: " + strconv.FormatBool(status) + "\n")
+	archive.WriteString(time.Now().Format("2006-01-02 15:04:05") + " - " + website + " | online: " + strconv.FormatBool(status) + "\n")
 
 	archive.Close()
 }
@@ -138,7 +138,7 @@ func readLog() {
 	archive, err := os.ReadFile("log.txt")
 
 	if err != nil {
-		fmt.Println("Ocorreu um erro:", err)
+		fmt.Println("An error occurred:", err)
 	}
 
 	fmt.Println(string(archive))
